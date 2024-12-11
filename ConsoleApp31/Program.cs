1ddff871dc1a4507d74d51f1fa66fe1c59affdc0
Ghiorghi777 <goncargeorgij9@gmail.com>
2024-12-11 09:29:05 +0200

    Add files via upload

---
+++ ConsoleApp31/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    internal class Program
+    {/// <summary>
+     /// По номеру месяца вывести название времени года.
+     /// </summary>
+     /// <param name="args"></param>
+
+
+        static void Main(string[] args)
+    {
+        byte month = InPut("Введите число от 1 до 12: ");
+        string m = GetMonth(month);
+        Console.WriteLine(m);
+        Console.ReadKey();
+
+    }
+    static byte InPut(string message)
+    {
+        Console.Write(message);
+        return byte.Parse(Console.ReadLine());
+    }
+    static string GetMonth(int month)
+    {
+            switch (month)
+            {
+                case 1: return "Зима";
+                case 2: return "Зима";
+                case 3: return "Весна";
+                case 4: return "Весна";
+                case 5: return "Весна";
+                case 6: return "Лето";
+                case 7: return "Лето";
+                case 8: return "Лето";
+                case 9: return "Осень";
+                case 10: return "Осень";
+                case 11: return "Осень";
+                case 12: return "Зима";
+                default: return "Ошибка";
+            }
+
+        }
+    }
+
+}
